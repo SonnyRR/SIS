66687a177f3f4c8ff950697c0f45e57910759472
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-05-02 11:02:56 +0300

    Added enum extension method and attributes.

--- SIS.HTTP/Enums/HttpResponseStatusCode.cs
+++ SIS.HTTP/Enums/HttpResponseStatusCode.cs
@@ -1,15 +1,26 @@
-﻿namespace SIS.HTTP.Enums
+﻿using System.ComponentModel;
+
+namespace SIS.HTTP.Enums
 {
     public enum HttpResponseStatusCode
     {
+        [Description("OK")]
         Ok = 200,
         Created = 201,
         Found = 302,
+
+        [Description("See Other")]
         SeeOther = 303,
+
+        [Description("Bad Request")]
         BadRequest = 400,
         Unauthorized = 401,
         Forbidden = 403,
+
+        [Description("Not Found")]
         NotFound = 404,
+
+        [Description("Internal Server Error")]
         InternalServerError = 500
     }
 }
---
+++ SIS.HTTP/Extensions/HttpResponseStatusExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+using SIS.HTTP.Enums;
+
+namespace SIS.HTTP.Extensions
+{
+    public static class HttpResponseStatusExtensions
+    {
+        public static string GetResponseLine(this HttpResponseStatusCode value)
+        {
+            FieldInfo fieldInfo = value
+                .GetType()
+                .GetField(value.ToString());
+
+            var attributes = (DescriptionAttribute[])fieldInfo
+                .GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            if (attributes.Length > 0)
+                return attributes[0].Description;
+
+            return value.ToString();
+        }
+    }
+}
