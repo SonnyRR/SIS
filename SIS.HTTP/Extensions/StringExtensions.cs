88003c11a1a0b6d46cd7024adac302b43cc31564
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-05-02 11:11:22 +0300

    Added string extensions.

---
+++ SIS.HTTP/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace SIS.HTTP.Extensions
+{
+    using System.Globalization;
+
+    public static class StringExtensions
+    {
+        public static string Capitalize(this string @string)
+        {
+            return CultureInfo.InvariantCulture.TextInfo.ToTitleCase(@string);
+        }
+    }
+}
