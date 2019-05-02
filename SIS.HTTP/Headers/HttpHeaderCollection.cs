cbf5ff9938ac5b009ae2e0130c690d6897becf9b
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-05-02 12:14:23 +0300

    Added HttpHeader class, contracts and HttpHeaderCollection.

---
+++ SIS.HTTP/Headers/HttpHeaderCollection.cs
@@ -0,0 +1,37 @@
+﻿namespace SIS.HTTP.Headers
+{
+    using System;
+    using System.Collections.Generic;
+
+    using SIS.HTTP.Headers.Contracts;
+
+    public class HttpHeaderCollection : IHttpHeaderCollection
+    {
+        private readonly IDictionary<string, HttpHeader> headers;
+
+        public HttpHeaderCollection()
+        {
+            this.headers = new Dictionary<string, HttpHeader>();
+        }
+
+        public void Add(HttpHeader header)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsHeader(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HttpHeader GetHeader(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
