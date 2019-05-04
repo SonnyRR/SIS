e6ae3b145055b5ad48a5592fa930760a096ab0fa
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-05-04 11:34:20 +0300

    Implemented IsRequestLineValid() and added ParseRequest().

---
+++ SIS.HTTP/Requests/HttpRequest.cs
@@ -0,0 +1,70 @@
+﻿namespace SIS.HTTP.Requests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using SIS.HTTP.Enums;
+    using SIS.HTTP.Exceptions;
+    using SIS.HTTP.Headers;
+    using SIS.HTTP.Requests.Contracts;
+
+    public class HttpRequest : IHttpRequest
+    {
+        public HttpRequest(string requestAsString)
+        {
+            this.FormData = new Dictionary<string, object>();
+            this.QueryData = new Dictionary<string, object>();
+            this.Headers = new HttpHeaderCollection();
+        }
+
+        public string Path { get; private set; }
+
+        public string Url { get; private set; }
+
+        public Dictionary<string, object> FormData { get; }
+
+        public Dictionary<string, object> QueryData { get; }
+
+        public HttpHeaderCollection Headers { get; }
+
+        public HttpRequestMethod RequestMethod { get; private set; }
+
+        private void ParseRequest(string requestString)
+        {
+            string[] request = requestString
+                .Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
+                [0]
+                .Trim()
+                .Split();
+
+            if (this.IsRequestLineValid(request))
+                throw new BadRequestException();
+
+        }
+
+        private bool IsRequestLineValid(string[] requestLineArgs)
+        {
+            bool isValid = true;
+
+            var method = CultureInfo
+                .InvariantCulture
+                .TextInfo
+                .ToTitleCase(requestLineArgs[0]);
+
+            var route = requestLineArgs[1];
+            var protocol = requestLineArgs[2];
+
+            if (!Enum.TryParse<HttpRequestMethod>(method, out _))
+                isValid = false;
+
+            else if (!Uri.IsWellFormedUriString(route, UriKind.Absolute))
+                isValid = false;
+
+            else if (protocol != "HTTP/1.1")
+                isValid = false;
+                
+            return isValid;
+        }
+    }
+
+}
