2b493807214ea10ac7178b4da9fe0ea6eda090c7
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-05-06 15:38:16 +0300

    Updated Server and created ConnectionHandler.

---
+++ SIS.WebServer/ConnectionHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace SIS.WebServer
+{
+    using System.Net.Sockets;
+    using System.Threading.Tasks;
+    using SIS.HTTP.Requests.Contracts;
+    using SIS.WebServer.Routing;
+
+    internal class ConnectionHandler
+    {
+        private Socket client;
+        private ServerRoutingTable routingTable;
+
+        public ConnectionHandler(Socket client, ServerRoutingTable routingTable)
+        {
+            this.client = client;
+            this.routingTable = routingTable;
+        }
+
+        private async Task<IHttpRequest> ReadRequest()
+        {
+
+        }
+    }
+}
--- SIS.WebServer/Server.cs
+++ SIS.WebServer/Server.cs
@@ -1,8 +1,49 @@
 ﻿namespace SIS.WebServer
 {
     using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Threading.Tasks;
+    using SIS.WebServer.Routing;
 
     public class Server
     {
+        private const string LocalhostIpAddress = "127.0.0.1";
+
+        private readonly int port;
+        private readonly TcpListener listener;
+        private readonly ServerRoutingTable routingTable;
+        private bool isRunning;
+
+        public Server(int port, ServerRoutingTable routingTable)
+        {
+            this.port = port;
+            this.routingTable = routingTable;
+
+            var ipAddress = IPAddress.Parse(LocalhostIpAddress);
+            this.listener = new TcpListener(ipAddress, this.port);
+
+        }
+
+        public void Run()
+        {
+            this.listener.Start();
+            this.isRunning = true;
+
+            Console.WriteLine($"SIS Server is running on: {LocalhostIpAddress}:{this.port} | {Environment.OSVersion}");
+
+            var task = Task.Run(this.ListenLoop);
+            task.Wait();
+        }
+
+        public async Task ListenLoop()
+        {
+            while (this.isRunning)
+            {
+                var client = await this.listener.AcceptSocketAsync();
+                var connectionHandler = new ConnectionHandler(client, this.routingTable);
+            }
+        }
+
     }
 }
