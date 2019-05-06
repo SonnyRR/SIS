a7091a68a7c0e9e9674c28e6cab0b56081a73a66
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-05-06 14:04:37 +0300

    Refactoring.

--- SIS.HTTP/Headers/Contracts/IHttpHeaderCollection.cs
+++ SIS.HTTP/Headers/Contracts/IHttpHeaderCollection.cs
@@ -5,7 +5,9 @@
     public interface IHttpHeaderCollection
     {
         void Add(HttpHeader header);
+
         bool ContainsHeader(string key);
+
         HttpHeader GetHeader(string key);
 
     }
