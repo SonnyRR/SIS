3802796ea67762502139a86072a5d3e92c88905b
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-05-06 14:07:16 +0300

    Added HttpResponse class.

---
+++ SIS.HTTP/Responses/HttpResponse.cs
@@ -0,0 +1,39 @@
+﻿namespace SIS.HTTP.Responses
+{
+    using System;
+
+    using SIS.HTTP.Enums;
+    using SIS.HTTP.Headers;
+    using SIS.HTTP.Headers.Contracts;
+    using SIS.HTTP.Responses.Contracts;
+
+    public class HttpResponse : IHttpResponse
+    {
+        public HttpResponse()
+        {
+        }
+
+        public HttpResponse(HttpResponseStatusCode statusCode)
+        {
+            this.Headers = new HttpHeaderCollection();
+            //this.Content = new byte[0];
+            this.StatusCode = statusCode;
+        }
+
+        public HttpResponseStatusCode StatusCode { get; set; }
+
+        public IHttpHeaderCollection Headers { get; private set; }
+
+        public byte[] Content { get; set; }
+
+        public void AddHeader(HttpHeader header)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] GetBytes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
