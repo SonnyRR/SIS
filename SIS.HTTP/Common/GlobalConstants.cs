3772d4af2f7392f58874cdc161e39c0a096bcede
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-05-14 13:58:52 +0300

    Added CoreValidator class and new const string in GlobalConstants.

--- SIS.HTTP/Common/GlobalConstants.cs
+++ SIS.HTTP/Common/GlobalConstants.cs
@@ -4,5 +4,6 @@
     {
         public const string HttpOneProtocolFragment = "HTTP/1.1";
         public const string HostHeaderKey = "Host";
+        public const string HttpNewLine = "\r\n";
     }
 }
