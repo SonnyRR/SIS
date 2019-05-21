e2303de8e44076b8a74fbd8f2171de5db06edf6f
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-05-21 14:53:38 +0300

    Added IHttpCookieCollection and HttpCookiecollection implementation.

---
+++ SIS.HTTP/Cookies/HttpCookieCollection.cs
@@ -0,0 +1,58 @@
+﻿namespace SIS.HTTP.Cookies
+{
+    using System.Collections;
+    using System.Collections.Generic;
+
+    using SIS.HTTP.Common;
+    using SIS.HTTP.Cookies.Contracts;
+
+    public class HttpCookieCollection : IHttpCookieCollection
+    {
+        private Dictionary<string, HttpCookie> cookies;
+
+        public HttpCookieCollection()
+        {
+            this.cookies = new Dictionary<string, HttpCookie>();
+        }
+
+        public void AddCookie(HttpCookie cookie)
+        {
+            CoreValidator.ThrowIfNull(cookie, nameof(cookie));
+
+            this.cookies[cookie.Key] = cookie;
+        }
+
+        public bool ContainsCookie(string key)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(key, nameof(key));
+
+            return this.cookies.ContainsKey(key);
+        }
+
+        public HttpCookie GetCookie(string key)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(key, nameof(key));
+
+            return this.cookies[key];
+        }
+
+        public IEnumerator<HttpCookie> GetEnumerator()
+        {
+            foreach (var kvp in this.cookies)
+            {
+                yield return kvp.Value;
+            }
+        }
+
+        public bool HasCookies()
+        {
+            return this.cookies.Count > 0 ? true : false;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            // FIXME
+            return (IEnumerator)this.GetEnumerator();
+        }
+    }
+}
