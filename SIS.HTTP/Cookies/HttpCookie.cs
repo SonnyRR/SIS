c274f1911e91ac2b73bce067e7a4f45bd526dff4
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-05-21 14:33:55 +0300

    Added HttpCookie.cs and updated projects target net core framework from 2.1 to 2.2

---
+++ SIS.HTTP/Cookies/HttpCookie.cs
@@ -0,0 +1,64 @@
+﻿using SIS.HTTP.Common;
+using System;
+using System.Text;
+
+namespace SIS.HTTP.Cookies
+{
+    public class HttpCookie
+    {
+
+        private const int HttpCookiesDefaultExpirationDays = 3;
+        private const string HttpCookieDefaultPath = "/";
+
+        public HttpCookie(string key, string value,
+            int expires = HttpCookiesDefaultExpirationDays, string path = HttpCookieDefaultPath)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(key, nameof(key));
+            CoreValidator.ThrowIfNullOrEmpty(value, nameof(value));
+
+            this.Key = key;
+            this.Value = value;
+            this.Expires = DateTime.UtcNow.AddDays(expires);
+            this.Path = path;
+            this.HttpOnly = true;
+        }
+
+        public HttpCookie(string key, string value, bool isNew,
+            int expires = HttpCookiesDefaultExpirationDays, string path = HttpCookieDefaultPath)
+            : this(key, value, expires, path)
+        {
+            this.IsNew = isNew;
+        }
+
+        public string Key { get; }
+
+        public string Value { get; }
+
+        public DateTime Expires { get; private set; }
+
+        public string Path { get; set; }
+
+        public bool IsNew { get; }
+
+        public bool HttpOnly { get; set; } 
+
+        public void Delete()
+        {
+            this.Expires = DateTime.UtcNow.AddDays(-1);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.Append($"{this.Key}={this.Value}; Expires={this.Expires:R}");
+
+            if (this.HttpOnly)
+                builder.Append($"; HttpOnly");
+
+            builder.Append($"; Path={this.Path}");
+
+            return builder.ToString();
+        }
+    }
+}
