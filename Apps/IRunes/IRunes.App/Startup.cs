16f5d92137f2241c56f39cbf685aabb069d2b4b5
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-05-29 20:03:43 +0300

    Added IRunes demo app.

---
+++ Apps/IRunes/IRunes.App/Startup.cs
@@ -0,0 +1,21 @@
+﻿namespace IRunes.App
+{
+    using IRunes.Data;
+    using SIS.MvcFramework;
+    using SIS.MvcFramework.Routing;
+
+    public class Startup : IMvcApplication
+    {
+        public void Configure(IServerRoutingTable serverRoutingTable)
+        {
+            using (var context = new RunesDbContext())
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+
+        public void ConfigureServices()
+        {
+        }
+    }
+}
