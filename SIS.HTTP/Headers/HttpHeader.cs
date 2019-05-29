1d3a36bd411786eacc03d0aa174914fc7bbc1e64
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-05-29 11:09:39 +0300

    Added const values to HttpHeader.cs.

--- SIS.HTTP/Headers/HttpHeader.cs
+++ SIS.HTTP/Headers/HttpHeader.cs
@@ -4,6 +4,11 @@ namespace SIS.HTTP.Headers
 {
     public class HttpHeader
     {
+        public const string Cookie = "Cookie";
+        public const string ContentType = "Content-Type";
+        public const string ContentLength = "Content-Length";
+        public const string ContentDisposition = "Content-Disposition";
+
         public HttpHeader(string key, string value)
         {
             CoreValidator.ThrowIfNullOrEmpty(key, nameof(key));
@@ -14,6 +19,7 @@ namespace SIS.HTTP.Headers
         }
 
         public string Key { get; }
+
         public string Value { get;}
 
         public override string ToString()
