5ea9814904038d984f075c42dac3dfe570408e7d
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-05-30 12:28:16 +0300

    Added HttpResponseTests and a simple test.

--- SIS.Tests/HTTP/HttpResponseTests.cs
+++ SIS.Tests/HTTP/HttpResponseTests.cs
@@ -1,10 +1,19 @@
-﻿using System;
-namespace SIS.Tests.HTTP
+﻿namespace SIS.Tests.HTTP
 {
+    using Xunit;
+    using SIS.HTTP.Responses;
+    using SIS.HTTP.Enums;
+
     public class HttpResponseTests
     {
-        public HttpResponseTests()
+        [Fact]
+        public void HttpResponseForbiddenToString()
         {
+            HttpResponse response = new HttpResponse(HttpResponseStatusCode.Forbidden);
+
+            var responseToString = response.ToString();
+
+            Assert.True(responseToString == "HTTP/1.1 403 Forbidden\r\n\r\n\r\n", "Forbidden response is not correctly represented as a string.");
         }
     }
 }
