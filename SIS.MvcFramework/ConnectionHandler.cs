25453dc7d4edf31d6cb8d79926b3e475d40aca82
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-06-04 14:52:54 +0300

    Small refactoring in ConnectionHandler.

--- SIS.MvcFramework/ConnectionHandler.cs
+++ SIS.MvcFramework/ConnectionHandler.cs
@@ -167,11 +167,11 @@
             }
             catch (BadRequestException e)
             {
-                httpResponse = new TextResult(e.Message, HttpResponseStatusCode.BadRequest);
+                httpResponse = new TextResult(e.Message.ToString(), HttpResponseStatusCode.BadRequest);
             }
             catch (Exception e)
             {
-                httpResponse = new TextResult(e.Message, HttpResponseStatusCode.InternalServerError);
+                httpResponse = new TextResult(e.Message.ToString(), HttpResponseStatusCode.InternalServerError);
             }
 
             await this.PrepareResponse(httpResponse);
