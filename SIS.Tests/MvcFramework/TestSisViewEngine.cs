2d837c6b278fc5702a3ac17789300006dc3b9bc7
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-06-05 12:46:58 +0300

    Added MVC framework tests.

---
+++ SIS.Tests/MvcFramework/TestSisViewEngine.cs
@@ -0,0 +1,36 @@
+﻿using SIS.MvcFramework.ViewEngine;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Xunit;
+
+namespace SIS.MvcFramework.Tests
+{
+    public class TestSisViewEngine
+    {
+        [Theory]
+        [InlineData("TestWithoutCSharpCode")]
+        [InlineData("UseForForeachAndIf")]
+        [InlineData("UseModelData")]
+        public void TestGetHtml(string testFileName)
+        {
+            IViewEngine viewEngine = new SisViewEngine();
+
+            var a = Environment.CurrentDirectory;
+            var viewFileName = Path.Combine("MvcFramework", "ViewTests", $"{testFileName}.html");
+            var expectedResultFileName = Path.Combine("MvcFramework", "ViewTests", $"{testFileName}.Result.html");
+
+            var viewContent = File.ReadAllText(viewFileName);
+            var expectedResult = File.ReadAllText(expectedResultFileName);
+
+            var actualResult = viewEngine.GetHtml<object>(viewContent, new TestViewModel()
+            {
+                StringValue = "str",
+                ListValues = new List<string> { "123", "val1", string.Empty },
+            }, new Identity.Principal() { });
+            Assert.Equal(expectedResult.TrimEnd(), actualResult.TrimEnd());
+        }
+    }
+}
